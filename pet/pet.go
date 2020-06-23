1de688e42db56ac0961621c6225c3c041401fb9c
liuchaofan <liuchaofan@didiglobal.com>
2020-06-23 20:42:53 +0800

    add skill

--- pet/pet.go
+++ pet/pet.go
@@ -6,27 +6,31 @@ import (
 )
 
 type attr struct {
-	num int
+	num   int
 	round int
 }
 type Status struct {
-	IsStop int
-
+	IsStop    int  // 是否停止行动n回合
+	IsReflect bool // 是否反弹伤害
+	AttUp     int
+	WeaUp     int
+	SpeUp     int
+	Fake      bool
+	Reduc     int
+	Recov     int
 }
 type pet struct {
-	name string
-	hp int
-	po int
-	ag int
-	sp int
-	level int
-	wps []int
-	sks []int
-	title string
+	name    string
+	hp      int
+	po      int
+	ag      int
+	sp      int
+	level   int
+	wps     []int
+	sks     []int
+	title   string
 	addAttr map[string]attr
 }
-
-
 
 func Adopt(name string) *pet {
 	nUser := &pet{
@@ -40,23 +44,23 @@ func Adopt(name string) *pet {
 		sks:   nil,
 		title: "乐斗小菜",
 	}
-	for _, w := range Weapons  {
+	for _, w := range Weapons {
 		nUser.wps = append(nUser.wps, w.Id)
 	}
 	// todo 写入文件
 	return nUser
 }
 
-func (p *pet)LevelUp() {
+func (p *pet) LevelUp() {
 	p.hp += rand.Intn(10)
 	p.po += rand.Intn(3)
 	p.ag += rand.Intn(3)
 	p.sp += rand.Intn(3)
-	p.level ++
+	p.level++
 	// todo add title, 写入文件
 }
 
-func (p *pet)Pk(ep *pet) string {
+func (p *pet) Pk(ep *pet) string {
 	round := 1
 	res := ""
 	res += fmt.Sprintf(PK_TITLE, p.name, ep.name)
@@ -93,12 +97,11 @@ func (p *pet)Pk(ep *pet) string {
 				return res
 			}
 		}
-		round ++
+		round++
 	}
 }
 
-func (p *pet)_chooseWeapon() *weapon {
+func (p *pet) _chooseWeapon() *weapon {
 	w := rand.Intn(len(p.wps))
 	return &Weapons[w]
 }
-
